d6ea95e85c82749be7d5d98ccd6e44797757f68c
lleflour14 <louise.leflour@sfr.fr>
2024-11-13 13:44:11 +0100

    init commit

---
+++ app/src/main/java/com/example/examen/Screen1.kt
@@ -0,0 +1,12 @@
+package com.example.examen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun Screen1(navController: NavController) {
+    Text(
+        text = "Hello!"
+    )
+}
